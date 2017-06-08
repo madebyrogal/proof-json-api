90a3ca7f292374956306f6784628c7836d5f92b5
Tomek Rogalski <tomek@propertyfinder.ae>
2017-06-08 17:28:15 +0400

    ADD browserify supporting

--- js/data/transactionData.js
+++ js/data/transactionData.js
@@ -1,4 +1,4 @@
-var translaction300 = {
+var transaction300 = {
     "data": [
         {
             "type": "transaction",
@@ -9005,4 +9005,35 @@ var translaction300 = {
         "total": 321,
         "filtered": 300
     }
+}
+
+module.exports.transaction = transaction300
+
+module.exports.example = {
+    "data": [{
+        "type": "articles",
+        "id": "1",
+        "attributes": {
+            "title": "JSON API paints my bikeshed!",
+            "body": "The shortest article. Ever.",
+            "created": "2015-05-22T14:56:29.000Z",
+            "updated": "2015-05-22T14:56:28.000Z"
+        },
+        "relationships": {
+            "author": {
+                "data": {"id": "42", "type": "people"}
+            }
+        }
+    }],
+    "included": [
+        {
+            "type": "people",
+            "id": "42",
+            "attributes": {
+                "name": "John",
+                "age": 80,
+                "gender": "male"
+            }
+        }
+    ]
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
-var store = require('jsonapi-datastore')
+var data = require('./data/transactionData');
+var JsonApiDataStore = require('jsonapi-datastore/dist/node-jsonapi-datastore').JsonApiDataStore
+var JSONAPISerializer = require('json-api-serializer');
 
-console.log(store)
+
+console.info('Testing jsonapi-datastore')
+var store = new JsonApiDataStore();
+store.sync(data.transaction)
+var transactions = store.findAll('transaction');
+var transaction = store.find('transaction', 620);
+console.log('Transactions: ', transactions)
+console.log('Transaction: ', transaction)
+console.log('Get props from transaction: ', transaction.community_location_name)
+// store.sync(data.example)
+// var articles = store.findAll('articles');
+// var article = store.find('articles', 1);
+// console.log('articles: ', articles)
+// console.log('article: ', article)
+// console.log('Get props from article: ', article.author.name)
+// article.author.name = 'Tomek';
+// console.log('Get new props from article: ', article.author.name)
+// var serializeNew = article.serialize()
+// console.log('Serialize after changes: ', serializeNew)
+// store.sync(serializeNew)
+// console.log('Response with new articles :', store.find('articles', 1))
+
+console.info('Testing json-api-serializer')
+var Serializer = new JSONAPISerializer();
+Serializer.register('transaction', {});
+Serializer.register('articles', {});
+var transactions = Serializer.deserialize('transaction', data.transaction);
+console.log('Transactions: ', transactions)
+
